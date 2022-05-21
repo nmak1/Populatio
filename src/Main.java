2af20a372127edd1b646a3019e1f4f212c940ebd
nmak1 <nmak1@rambler.ru>
2022-05-21 15:54:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        persons.stream()
+                .filter(person -> person.getAge()<18)
+                .count();
+
+        persons.stream()
+                .filter(person -> person.getSex() == Sex.MAN)
+                .filter(person -> person.getAge()>=18)
+                .filter(person -> person.getAge()<=27)
+                .map(person ->  person.getFamily())
+                .collect(Collectors.toList());
+        persons.stream()
+                .filter(person -> person.getAge() > 18)
+                .filter(person -> person.getEducation(Education.HIGHER) == Education.HIGHER )
+                .filter(person -> person.getSex() == Sex.MAN & person.getAge() <= 65
+                        && person.getSex() == Sex.WOMAN & person.getAge() <=60)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+
+
+
+
+    }
+}
